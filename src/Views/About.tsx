aa2b19c64992988bc025886cf9509b910b0aa03d
Noefal Ahmed <noefalahmed@gmail.com>
2024-12-22 06:32:42 +0500

    updated home and about pages

--- src/Views/About.tsx
+++ src/Views/About.tsx
@@ -1,43 +1,94 @@
-import React from 'react';
-import styles from './Home.module.css';
-import styles2 from './Projects/Projects.module.css'
-import Button from '../components/Button';
-import buttonstyles from './About.module.css'
+import React from "react";
+import styles from "./Home.module.css";
+import styles2 from "./Projects/Projects.module.css";
+import Button from "../components/Button";
+import buttonstyles from "./About.module.css";
 
 const About: React.FC = () => {
   return (
     <div className={styles.contentContainer}>
-    <div className={styles2.subsection}>
-      <div className={styles2.sub2section}>
-        <div className={styles.typewriter} style={{textAlign:'left'}}>
-          <h1 className={styles.h1}>Noefal Ahmed</h1>
+      <div className={buttonstyles.heroSection}>
+        <div className={styles.heroline}>
+          <p className={styles.p2}>NOEFAL AHMED</p>
+          <p className={styles.p2}>REMOTE-FIRST PRODUCT DESIGNER</p>
         </div>
-        <h2 className={styles2.sh2}>Product Designer</h2>
+        <h1 className={buttonstyles.h1}>
+          <span >DESIGN</span>
+          <span> +</span>
+          <span > ENGINEERING</span>
+        </h1>
+
+      </div>
+
+      <div className={buttonstyles.workSection}>
+        <div className={buttonstyles.workItem}>
+          <img src="/assets/work1.png" className={buttonstyles.icon}></img>
+          <div className={styles2.sub2section}>
+            <h2 className={styles2.sh2}>UX STRATEGIC PLANNING</h2>
+            <p className={styles2.p1}>
+              In depth analysis of your business, competitive standards,
+              industry benchmarks, roadmap, and input parameters to develop a
+              design-centric strategy that addresses your unique challenges.
+            </p>
+          </div>
         </div>
+        <div className={buttonstyles.workItem}>
+          <img src="/assets/work2.png" className={buttonstyles.icon}></img>
+          <div className={styles2.sub2section}>
+            <h2 className={styles2.sh2}>USER EXPERIENCE & INTERFACE DESIGN</h2>
+            <p className={styles2.p1}>
+              End-to-end delivery of workflows that are well-research, defined,
+              iterated, prototyped, and measured to drive user retention and
+              customer satisfaction.
+            </p>
+          </div>
+        </div>
+        <div className={buttonstyles.workItem}>
+          <img src="/assets/work3.png" className={buttonstyles.icon}></img>
+          <div className={styles2.sub2section}>
+            <h2 className={styles2.sh2}>BRANDING</h2>
+            <p className={styles2.p1}>
+              From Logo to Packaging, Social media to Website, and Style Guides
+              to Assets, I provide authenticity and identity that attracts,
+              retains, and engages audiences.{" "}
+            </p>
+          </div>
+        </div>
+        <div className={buttonstyles.workItem}>
+          <img src="/assets/work4.png" className={buttonstyles.icon}></img>
+          <div className={styles2.sub2section}>
+            <h2 className={styles2.sh2}>DESIGN TO CODE</h2>
+            <p className={styles2.p1}>
+              Component Libraries, Spec Sheets, Design Tokens, Grid Systems, and
+              Responsiveness.
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+      <div className={buttonstyles.contactSection}>
+
         <p className={styles2.p1}>
-        I am a Mediator and Organiser at heart; I love the feeling that comes with making sense of things, whether it’s designing user experiences, bringing them to life using code, or creating a product from the ground up.         The intersection between Design and Engineering is where I found my footing and honed my skillset. Today, as I seek Senior Design roles, I’m equipped with a greater understanding of design as a function of a business.
-        </p>
-      </div>
-      {/* Placeholder for future sections */}
-      <div className={styles.workSection}>
-        {/* Career progression diagram will go here */}
-        {/* Calendar component will go here */}
-        {/* Custom schedule component will go here */}
-      </div>
-      <a className={buttonstyles.navButton} href="https://calendly.com/noefalahmed" target="_blank" rel="noopener noreferrer">
-            <Button variant="text-only" 
-                label="Request Call" 
-                isActive={false}
-                onlight={true}/>
-      </a>
-
-      {/* <div className={styles2.sub2section}>
-      <h2 className={styles.preheading}>Career Progression</h2>
-      <img src="/assets/cp1.png" className={`${styles2.image} ${styles2['image-dynamic']}`}></img>
-
-
-      </div> */}
-
+        Making sense of things is what I do best, whether it’s designing user experiences,
+          bringing them to life using code, or creating a product from the
+          ground up. Today, as I seek Senior Design roles, I’m equipped
+          with a greater understanding of design as a function of a business. 
+          Feel free to reach out!</p>
+        <a
+            className={buttonstyles.navButton}
+            href="https://calendly.com/noefalahmed"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button
+              variant="text-only"
+              label="Request Call"
+              isActive={false}
+              onlight={true}
+            />
+          </a>
+       </div>
     </div>
   );
 };
