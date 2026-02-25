0bd18fa4a56ff72e26b87c8ac56f613bda64404f
Noefal Ahmed <noefalahmed@gmail.com>
2026-02-25 14:35:48 -0500

    V 10.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import './index.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer.tsx';
 import DesignSystems from './Views/Projects/DesignSystems.tsx'
-import IconPack from './Views/Projects/IconPack.tsx';
 import DesignThinking from './Views/Projects/DesignThinking.tsx';
 import Guide from './Views/Projects/Guide.tsx';
 import Arine from './Views/Projects/Arine.tsx';
@@ -24,7 +23,6 @@ const App: React.FC = () => {
           {/* Define routes to render specific components based on URL */}
           <Route path="/" element={<Home />} />       
           <Route path="/design-systems" element={<DesignSystems />} />
-          <Route path="/icon-pack" element={<IconPack />} /> 
           <Route path="/guide" element={<Guide />} /> 
           <Route path="/design-thinking" element={<DesignThinking />} /> 
           <Route path="/arine" element={<Arine />} /> 
